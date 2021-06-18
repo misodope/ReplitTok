88a8b61f1317a351175d8590ce01efccc4fef692
Jerry Chen <misodope@cap-rx.com>
2021-06-17 20:10:44 -0400

    Whoop

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import "./index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,84 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import classNames from "classnames";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
-export default function Home() {
+import "react-circular-progressbar/dist/styles.css";
+import "animate.css";
+
+const LeftSide = ({ clicked }) => {
+  const leftText = "[a";
+  const leftClass = classNames("text-9xl animate__animated animate__fadeInLeft animate__delay-2s", {
+    animate__fadeOutUp: clicked,
+  });
+  return <div className={leftClass}>{leftText}</div>;
+};
+
+const RightSide = ({ clicked }) => {
+  const rightText = "s]";
+  const rightClass = classNames("text-9xl animate__animated animate__fadeInRight animate__delay-2s", {
+    animate__fadeOutUp: clicked,
+  });
+  return <div className={rightClass}>{rightText}</div>;
+};
+
+const Home = () => {
+  const [clicked, setClicked] = useState(false);
+  const [intervalId, setIntervalId] = useState();
+  const [loaderValue, setLoaderValue] = useState(0);
+  const [loaderClass, setLoaderClass] = useState("");
+
+  const onEnter = (e) => {
+    e.preventDefault();
+
+    // setClicked(!clicked);
+  };
+
+  useEffect(() => {
+    if (!loaderValue) {
+      const timer = setInterval(() => {
+        setLoaderValue((prev) => prev + 1);
+      }, 10);
+
+      setIntervalId(timer);
+    } else if (loaderValue === 100) {
+      setLoaderClass(
+        classNames({
+          "animate__animated animate__fadeOut animate__delay-1.5s": loaderValue === 100,
+        }),
+      );
+
+      setTimeout(() => {
+        setLoaderClass("poo");
+      }, 1500);
+
+      clearInterval(intervalId);
+    }
+  }, [loaderValue]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div onClick={onEnter} className="flex items-center min-h-screen justify-center">
+      <div style={{ width: 200, height: 200 }}>
+        <CircularProgressbar
+          value={loaderValue}
+          maxValue={100}
+          text={`${loaderValue}%`}
+          styles={buildStyles({
+            pathColor: `rgba(0, 0, 0, ${loaderValue / 100})`,
+            textColor: "#000",
+            trailColor: "#fff",
+            backgroundColor: "#000",
+            width: 200,
+            height: 200,
+          })}
+          className={loaderClass}
+        />
+        <div className="flex items-center justify-center py-2">
+          {loaderValue === 100 && <LeftSide clicked={clicked} />}
+          {loaderValue === 100 && <RightSide clicked={clicked} />}
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
